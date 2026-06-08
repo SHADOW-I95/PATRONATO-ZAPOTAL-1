b4145da51eb2eb11a653e83c307055a6c5dbbb45
marielayanorisvalle-ctrl <marielayanorisvalle@gmail.com>
2026-06-07 22:24:48 -0600

    Subir cambios

---
+++ Usuarios/pages/incio_seccion/seccion/seccion.js
@@ -0,0 +1,11 @@
+function mostrarPassword() {
+
+    let password = document.getElementById("password");
+
+    if(password.type === "password"){
+        password.type = "text";
+    }else{
+        password.type = "password";
+    }
+
+}
