75d8987d4dc4f8a726d4f4f844225419a129d8be
EC8027180-CTRL <ec8027180@gmail.com>
2026-06-19 11:02:14 -0600

    wea

--- Empleados/conexion.php
+++ Empleados/conexion.php
@@ -1,18 +1,10 @@
 <?php
-<<<<<<< HEAD:Usuarios/pages/incio_seccion/CONEXION.PHP
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "patronato";          
-$conexion = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-=======
 $host = "localhost";      
 $usuario = "root";        
 $clave = "";              
 $bd = "patronato";          
 
 $conexion = new mysqli($host, $usuario, $clave, $bd);
->>>>>>> 30b2d88f1b2bf8dda3174a6da6e6e2efcc768347:Empleados/conexion.php
 $conexion->set_charset("utf8");
 
 if ($conexion->connect_error) {
