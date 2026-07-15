3e5d22cff09fb8b1db590fe8b6758449f90c2302
alexasislaba08-hash <alexasislaba08@gmail.com>
2026-07-15 14:42:49 -0600

    ALEXAA

--- pagina de inicio/Pagina_inicio/includes/section4.php
+++ pagina de inicio/Pagina_inicio/includes/section4.php
@@ -1,35 +1,31 @@
 <?php
-
 $conexion = mysqli_connect("localhost","root","","patronato");
+
+if(!$conexion){
+    die("Error de conexión: " . mysqli_connect_error());
+}
 
 if(isset($_POST['enviar'])){
 
     $tipo = $_POST['tipo_reporte'];
     $descripcion = $_POST['descripcion'];
 
-    echo $tipo;
-    echo "<br>";
-    echo $descripcion;
-
-    $sql = "INSERT INTO reportes(id_tipo_reporte, descripcion)
+    $sql = "INSERT INTO reportes (id_tipo_reporte, descripcion)
             VALUES ('$tipo', '$descripcion')";
 
     if(mysqli_query($conexion, $sql)){
-        echo "GUARDADO";
+        echo "<script>alert('Reporte guardado correctamente');</script>";
     }else{
-        echo mysqli_error($conexion);
+        echo "Error: " . mysqli_error($conexion);
     }
 }
 ?>
 
 <div class="contenedor">
-
     <h2>Reportar Problema</h2>
 
     <form method="POST">
-
         <label>Tipo de Reporte</label>
-
         <select name="tipo_reporte" required>
             <option value="">Seleccione una opción</option>
             <option value="1">Fuga de Agua</option>
@@ -40,17 +36,12 @@ if(isset($_POST['enviar'])){
         </select>
 
         <label>Descripción</label>
-
         <textarea
             name="descripcion"
             rows="5"
             placeholder="Describa el problema..."
             required></textarea>
 
-        <button type="submit" name="enviar">
-            Enviar Reporte
-        </button>
-
+        <button type="submit" name="enviar">Enviar Reporte</button>
     </form>
-
 </div>
